e23881dc71476e8fcb2474e88f499f72b39faf19
tbd.okarabeyoglu <ovunc.karabeyoglu@teamdefinex.com>
2020-09-13 18:16:01 +0300

    gradle springboot project created

---
+++ caseinterview/src/main/java/com/trendyol/caseinterview/CaseinterviewApplication.java
@@ -0,0 +1,13 @@
+package com.trendyol.caseinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseinterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaseinterviewApplication.class, args);
+	}
+
+}
